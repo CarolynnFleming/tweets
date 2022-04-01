75127a882ff065714d0148f92e49bbc94356adad
CarolynnFleming <cfcatpink@gmail.com>
2022-04-01 15:24:40 -0700

    findbyusername function cannot log you in without a username

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const User = require('../lib/models/Github');
 
 jest.mock('../lib/utils/github');
 

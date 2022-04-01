cdc44dd1d352667b8da27c17062da84ca7c46b83
CarolynnFleming <cfcatpink@gmail.com>
2022-04-01 10:45:42 -0700

    first test written to redirect the outh page upon login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,32 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
-describe('alchemy-app routes', () => {
+jest.mock('../lib/middleware/authenticate.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'test_user',
+      photoUrl: 'http://image.com/image.png',
+    };
+    next();
+  };
+});
+
+describe('github-oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should redirect to the github oauth oage upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
   });
 });

60dbe2a6bf4ce0488a16f7874a969e3df5bf001c
CarolynnFleming <cfcatpink@gmail.com>
2022-04-06 17:39:31 -0700

    sql table to create tweets

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS tweets;
 
 CREATE TABLE github_users (
     username TEXT NOT NULL,
     email TEXT NOT NULL,
     photo TEXT NOT NULL
-)
+);
+
+CREATE TABLE tweets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    text TEXT NOT NULL,
+    username TEXT REFERENCES users(github_username)
+);
